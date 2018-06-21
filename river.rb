4ab2fa54d5e17ff58e6b38510b211e6a19ac72cb
EmilyHenken <20henkee@corjesu.org>
2018-06-21 21:01:34 +0000

    automatically backed up by learn

--- river.rb
+++ river.rb
@@ -8,12 +8,12 @@ class River
   end
 end
 
-nile.=River.new("The Nile", "4,258", "Egypt")
-mississippi.=River.new("The Mississippi", "2,320", "U.S.A")
-amazon.=River.new("The Amazon", "4,345", "Brazil")
-seine.=River.new("The Seine", "482", "Paris")
-yangtze.=River.new("The Yangtze", "3,951", "China")
-euphrates.=River.new("The Euphrates", "1,740", "Syria, Turkey, Iraq")
+nile = River.new("The Nile", "4,258", "Egypt")
+mississippi = River.new("The Mississippi", "2,320", "U.S.A")
+amazon = River.new("The Amazon", "4,345", "Brazil")
+seine = River.new("The Seine", "482", "Paris")
+yangtze = River.new("The Yangtze", "3,951", "China")
+euphrates = River.new("The Euphrates", "1,740", "Syria, Turkey, Iraq")
 
 puts nile.length
 puts amazon.length
