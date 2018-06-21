e605c4a2c8b22e48973868834f8e0c6ac7af2e3e
EmilyHenken <20henkee@corjesu.org>
2018-06-21 20:57:45 +0000

    automatically backed up by learn

--- river.rb
+++ river.rb
@@ -10,7 +10,7 @@ end
 
 nile. = River.new("The Nile", "Egypt")
 mississippi. = River.new("The Mississippi", "U.S.A")
-amazon. = River.new("The Amazon", "Brazil")
+amazon. = River.new("The Amazon", "Brazil", "4,245")
 seine. = River.new("The Seine", "Paris" "482")
 yangtze. = River.new("The Yangtze", "China", "3,951")
 euphrates. = River.new("The Euphrates", "1,740", "Syria, Turkey, Iraq")
