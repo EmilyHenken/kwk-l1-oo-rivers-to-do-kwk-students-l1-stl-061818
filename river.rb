8b9e8aa307b86705560230635eaffa87ade451ca
EmilyHenken <20henkee@corjesu.org>
2018-06-21 20:56:57 +0000

    automatically backed up by learn

--- river.rb
+++ river.rb
@@ -1,18 +1,17 @@
 class River
-  attr_accessor :name, :length, :countries, :discharge
+  attr_accessor :name, :length, :countries
   
-  def initialize(name, length, countries, discharge)
+  def initialize(name, length, countries)
     @name = name
     @length = length
     @countries = countries
-    @discharge = discharge
   end
 end
 
 nile. = River.new("The Nile", "Egypt")
 mississippi. = River.new("The Mississippi", "U.S.A")
 amazon. = River.new("The Amazon", "Brazil")
-seine. = River.new("The Seine")
+seine. = River.new("The Seine", "Paris")
 yangtze. = River.new("The Yangtze", "China")
-euphrates. = River.new("The Euphrates", "1,740", "Syria, Turkey, Iraq", "356 m^3/s")
+euphrates. = River.new("The Euphrates", "1,740", "Syria, Turkey, Iraq")
     
